a72a266ec4059956f7f2f5b855f9732f0a3938e1
Joe Anzalone <Joe@Shmit.com>
2012-11-28 15:04:11 -0500

    Add comments

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+// Require express, http, and socket.io
+var express = require('express'),
+	app = require('express')(),
+	server = require('http').createServer( app ),
+	io = require('socket.io').listen( server );
 
-var express = require('express');
-
+// Make all files under the "static" directory public
+// (Includes client-side HTML, CSS, and JavaScript)
 app.use( express.static('static') );
 
+// Listen for incoming connections on port 80
 server.listen(80);
 
+
 io.sockets.on('connection', function (socket) {
-  // socket.emit('news', { hello: 'world' });
-  socket.on('chat', function (data) {
-    // console.log(data);
-     io.sockets.emit('chat', data);
-  });
+// Run when receiving an incoming connection
+
+	socket.on('chat', function (data) {
+	// Run when receiving an incoming message on the "chat" event
+		// Relay chat message back to all clients on the "chat" event
+    	io.sockets.emit('chat', data);
+	});
+
 });
